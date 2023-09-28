8243a489328e6470003ae6361d3f6c637d338bf3
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-09-28 14:45:48 +0800

    feat: refactor getData to getQuran

--- src/data/index.ts
+++ src/data/index.ts
@@ -19,7 +19,7 @@ export const quranSchema = z.object({
 
 export type Quran = z.infer<typeof quranSchema>;
 
-export const getData = () => {
+export const getQuran = () => {
   const parse = z
     .object({
       verses: z.array(quranSchema),
@@ -33,8 +33,8 @@ export const getData = () => {
   return parse.data.verses;
 };
 
-export const getManyVerseIdBySurah = (select: number[]) => {
-  return getData().reduce((result: number[], item) => {
+export const getManyVerseIdBySurahId = (select: number[]) => {
+  return getQuran().reduce((result: number[], item) => {
     if (select.includes(item.chapter_id)) {
       result.push(item.id);
     }
@@ -42,8 +42,8 @@ export const getManyVerseIdBySurah = (select: number[]) => {
   }, []);
 };
 
-export const getManyVerseIdByJuz = (select: number[]) => {
-  return getData().reduce((result: number[], item) => {
+export const getManyVerseIdByJuzId = (select: number[]) => {
+  return getQuran().reduce((result: number[], item) => {
     if (select.includes(item.juz_number)) {
       result.push(item.id);
     }
@@ -52,11 +52,148 @@ export const getManyVerseIdByJuz = (select: number[]) => {
 };
 
 export const getVerseTextById = (id: number) => {
-  const find = getData().find((item) => item.id === id);
+  const find = getQuran().find((item) => item.id === id);
 
   if (!find) {
     throw new Error("Failed to find verse");
   }
 
   return find.text_uthmani;
+};
+
+const chapterNames = [
+  "Al-Fatihah",
+  "Al-Baqarah",
+  "Ali 'Imran",
+  "An-Nisa",
+  "Al-Ma'idah",
+  "Al-An'am",
+  "Al-A'raf",
+  "Al-Anfal",
+  "At-Tawbah",
+  "Yunus",
+  "Hud",
+  "Yusuf",
+  "Ar-Ra'd",
+  "Ibrahim",
+  "Al-Hijr",
+  "An-Nahl",
+  "Al-Isra",
+  "Al-Kahf",
+  "Maryam",
+  "Taha",
+  "Al-Anbya",
+  "Al-Hajj",
+  "Al-Mu'minun",
+  "An-Nur",
+  "Al-Furqan",
+  "Ash-Shu'ara",
+  "An-Naml",
+  "Al-Qasas",
+  "Al-'Ankabut",
+  "Ar-Rum",
+  "Luqman",
+  "As-Sajdah",
+  "Al-Ahzab",
+  "Saba",
+  "Fatir",
+  "Ya-Sin",
+  "As-Saffat",
+  "Sad",
+  "Az-Zumar",
+  "Ghafir",
+  "Fussilat",
+  "Ash-Shuraa",
+  "Az-Zukhruf",
+  "Ad-Dukhan",
+  "Al-Jathiyah",
+  "Al-Ahqaf",
+  "Muhammad",
+  "Al-Fath",
+  "Al-Hujurat",
+  "Qaf",
+  "Adh-Dhariyat",
+  "At-Tur",
+  "An-Najm",
+  "Al-Qamar",
+  "Ar-Rahman",
+  "Al-Waqi'ah",
+  "Al-Hadid",
+  "Al-Mujadila",
+  "Al-Hashr",
+  "Al-Mumtahanah",
+  "As-Saf",
+  "Al-Jumu'ah",
+  "Al-Munafiqun",
+  "At-Taghabun",
+  "At-Talaq",
+  "At-Tahrim",
+  "Al-Mulk",
+  "Al-Qalam",
+  "Al-Haqqah",
+  "Al-Ma'arij",
+  "Nuh",
+  "Al-Jinn",
+  "Al-Muzzammil",
+  "Al-Muddaththir",
+  "Al-Qiyamah",
+  "Al-Insan",
+  "Al-Mursalat",
+  "An-Naba",
+  "An-Nazi'at",
+  "'Abasa",
+  "At-Takwir",
+  "Al-Infitar",
+  "Al-Mutaffifin",
+  "Al-Inshiqaq",
+  "Al-Buruj",
+  "At-Tariq",
+  "Al-A'la",
+  "Al-Ghashiyah",
+  "Al-Fajr",
+  "Al-Balad",
+  "Ash-Shams",
+  "Al-Layl",
+  "Ad-Duhaa",
+  "Ash-Sharh",
+  "At-Tin",
+  "Al-'Alaq",
+  "Al-Qadr",
+  "Al-Bayyinah",
+  "Az-Zalzalah",
+  "Al-'Adiyat",
+  "Al-Qari'ah",
+  "At-Takathur",
+  "Al-'Asr",
+  "Al-Humazah",
+  "Al-Fil",
+  "Quraysh",
+  "Al-Ma'un",
+  "Al-Kawthar",
+  "Al-Kafirun",
+  "An-Nasr",
+  "Al-Masad",
+  "Al-Ikhlas",
+  "Al-Falaq",
+  "An-Nas",
+];
+
+export const getSurahByVerseId = (verseId: number) => {
+  const findVerseId = getQuran().find((item) => item.id === verseId);
+
+  if (!findVerseId) {
+    throw new Error("Failed to find verse");
+  }
+
+  return {
+    id: findVerseId.chapter_id,
+    name: chapterNames[findVerseId.chapter_id - 1],
+  };
+};
+
+export const getSurahBySurahId = (surahId: number) => {
+  return {
+    id: surahId,
+    name: chapterNames[surahId - 1],
+  };
 };
--- src/test/data.test.ts
+++ src/test/data.test.ts
@@ -1,15 +1,21 @@
-import { getData, getManyVerseIdBySurah, getVerseTextById } from "~/data";
+import {
+  getManyVerseIdBySurahId,
+  getQuran,
+  getSurahBySurahId,
+  getSurahByVerseId,
+  getVerseTextById,
+} from "~/data";
 
 describe("getData", () => {
   it("should return array of data from quran.json with length 6236", () => {
-    const data = getData();
+    const data = getQuran();
     expect(data).toHaveLength(6236);
   });
 });
 
 describe("getManyVersesIdBySurah", () => {
   it("should return array of id", () => {
-    const data = getManyVerseIdBySurah([1, 2]);
+    const data = getManyVerseIdBySurahId([1, 2]);
     expect(data).toHaveLength(7 + 286);
   });
 });
@@ -18,5 +24,25 @@ describe("getVerseTextById", () => {
   it("should return text", () => {
     const data = getVerseTextById(2);
     expect(data).toBe("ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَـٰلَمِينَ");
+  });
+});
+
+describe("getSurahByVerseId", () => {
+  it("should return surah", () => {
+    const data = getSurahByVerseId(10);
+    expect(data).toEqual({
+      id: 2,
+      name: "Al-Baqarah",
+    });
+  });
+});
+
+describe("getSurahBySurahId", () => {
+  it("should return surah", () => {
+    const data = getSurahBySurahId(2);
+    expect(data).toEqual({
+      id: 2,
+      name: "Al-Baqarah",
+    });
   });
 });
